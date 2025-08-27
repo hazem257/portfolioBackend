101225f246ff7b393a5cc154c2f771b30c2cfd94
‪Hazem Gamal <161766768+hazem257@users.noreply.github.com>
2025-08-27 22:12:35 +0300

    Delete src directory

--- src/certificates/dto/create-certificate.dto.ts
+++
@@ -1,6 +0,0 @@
-export class CreateCertificateDto {
-  title: string;
-  description?: string;
-  company: string;
-  date?: string;
-}
--- src/cloudinary/cloudinary.config.ts
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from 'cloudinary';
-
-cloudinary.config({
-  cloud_name: 'dybyowe82',
-  api_key: '987257745629334',
-  api_secret: 'MU3tycW6da1hiGNoK2PXAkFSdT4',
-});
-
-export default cloudinary;
